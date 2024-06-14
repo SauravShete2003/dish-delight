9b6d3117d7f3c3d4107556d6a26b3a854a00ddaf
pranayrdandekar7 <pranayrdandekar7@gmail.com>
2024-06-14 23:40:51 +0530

    logic for like button added

---
+++ src/components/VegCard/VegCard.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import "./VegCard.css"
+import ImgHeartUnfilled from "./heart-unfilled.png"
+import ImgHeartFilled from "./heart-filled.png"
+import ImgClock from "./clock.png"
+import Button from "./../Button/Button"
+
+function VegCard({ title, image, description, time }) {
+    const [liked, setLiked] = useState(false)
+
+    return (
+        <div className="card-container">
+            <img 
+                src={liked ? ImgHeartFilled : ImgHeartUnfilled} 
+                className="like-image" id="like" 
+                onClick={() => {
+                    setLiked(!liked)
+                }} 
+            />
+            <img src={image} className="card-image" />
+
+            <h1 className="card-title">{title}</h1>
+
+            <p className="card-disciption">{description.substring(0, 150)}...</p>
+
+            <div className="time-container">
+                <img src={ImgClock} className="img-clock" />
+                <span className="time">{time}</span>
+            </div>
+
+            <div className="category-card-button">
+                <Button text={"Know More"} />
+            </div>
+        </div>
+    )
+}
+
+export default VegCard
--- src/views/Categories/CategoriesVeg.js
+++ src/views/Categories/CategoriesVeg.js
@@ -1,10 +1,8 @@
 import "./CategoriesVeg.css"
 import Data from "../../data/CategoriesVegData/CategoriesVesdata"
-import ImgLike1 from "../../assets/CategoriesVeg/heart.png"
-import ImgClock from "../../assets/CategoriesVeg/clock.png"
-import Button from "../../components/Button/Button"
+import VegCard from "../../components/VegCard/VegCard"
 
-function VegCard() {
+function CategoriesVeg() {
 
   return (
     <>
@@ -20,44 +18,13 @@ function VegCard() {
             // categories
           } = blogObject
 
-
-          return (
-            <div className="card-container">
-
-              <img src={ImgLike1} className="like-image" />
-              <img src={Image} className="card-image" />
-
-              <h1 className="card-title">{title}</h1>
-
-              <p className="card-disciption">{Discription.substring(0, 150)}...</p>
-
-              <div className="time-container">
-                <img src={ImgClock} className="img-clock" />
-                <span className="time">{time}</span>
-
-
-                 {/* <span className="categories-name">{categories} </span>  */}
-              </div>
-
-              <div className="category-card-button">
-                <Button text={"Know More"} />
-                </div>
-
-
-              </div>
-
-              )
+          return (<VegCard title={title} image={Image} description={Discription} time={time} />)
         })}
-            </div>
-
-
-
+      </div>
 
     </>
-      )
+  )
 }
 
-
-
-      export default VegCard
+export default CategoriesVeg
 
