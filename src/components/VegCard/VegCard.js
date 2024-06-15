5f8ad4bd57e057ca42b66ef6eed366dab09c3cd7
pranayrdandekar7 <pranayrdandekar7@gmail.com>
2024-06-15 14:54:19 +0530

    changes added in vegcard

--- src/components/VegCard/VegCard.js
+++ src/components/VegCard/VegCard.js
@@ -10,7 +10,7 @@ function VegCard({ id,title, image, description, time }) {
     const [liked, setLiked] = useState(false)
 
     return (
-        <Link className="card-container" to={`/recipe/${id}`}>
+        <div className="card-container" to={`/recipe/${id}`}>
             <img 
                 src={liked ? ImgHeartFilled : ImgHeartUnfilled} 
                 className="like-image" id="like" 
@@ -29,10 +29,10 @@ function VegCard({ id,title, image, description, time }) {
                 <span className="time">{time}</span>
             </div>
 
-            <div className="category-card-button">
+            <Link className="category-card-button" to={`/recipe/${id}`}>
                 <Button text={"Know More"} />
-            </div>
-        </Link>
+            </Link>
+        </div>
     )
 }
 
