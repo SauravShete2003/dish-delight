a7e864d2d2305c1a15ff216fe6082611738733bf
shriyaborkar358 <shriyaborkar71@gmail.com>
2024-06-15 16:06:32 +0530

    changes added

--- src/views/Recipe/Recipe.js
+++ src/views/Recipe/Recipe.js
@@ -13,8 +13,8 @@ const selectedData = DataCard.find((dataObject)=>dataObject.id===id)
   return (<>
     <div>
       <h1 className='title text-center '>Recipe: {selectedData.title}</h1>
-      <img src={selectedData.Image} className="recipe-img  mx-auto d-block " />
-      <p className='recipe-description'>{selectedData.Discription}</p>
+      <img src={selectedData.image} className="recipe-img  mx-auto d-block " />
+      <p className='recipe-description'>{selectedData.description}</p>
       <h4 className='category'>Category:{selectedData.categories}</h4>
       <h1 className='category'>Proccess: {selectedData.title}</h1>
       <div>
