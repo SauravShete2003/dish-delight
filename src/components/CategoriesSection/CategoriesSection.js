cd83f980b3f0a031d71ccd9a6158f30869d1a7a0
sauravshete2003 <sauravshete72@gmail.com>
2024-06-15 15:24:39 +0530

    add chnage

--- src/components/CategoriesSection/CategoriesSection.js
+++ src/components/CategoriesSection/CategoriesSection.js
@@ -1,22 +1,14 @@
 import React from 'react';
-import BreakFast from "../../../assets/BreakFast.jpg";
-import Veg from "../../../assets/veg-img.jpeg";
-import NonVeg from "../../../assets/NonVeg.jpeg";
-import FastFood from "../../../assets/fast-food.jpg";
+import "./CategoriesSection.css"
+
 
 function CategoriesSection({ name, image, timing }) {
-  const homeImage = {
-    "BreakFast": BreakFast,
-    "Veg": Veg,
-    "Non-Veg": NonVeg,
-    "FastFood": FastFood,
-  };
 
   return (
     <div className="categories-section">
-      <h2>Explore Categories</h2>
+      
       <div className="categories-container">
-        <img src={homeImage[image]} alt={name} className="category-image" />
+        <img src={image} alt={name} className="category-image" />
         <span className="category-name">{name}</span>
         <span className="category-timing">{timing}</span>
       </div>
--- src/data/CategoriesVegData/CategoriesVesdata.js
+++ src/data/CategoriesVegData/CategoriesVesdata.js
@@ -1,59 +1,51 @@
 
 
 const DataCard = [
-    {
-   
-      title : "Dal Tadka",
-   
-      Image:"https://www.masalabox.com/wp-content/uploads/2023/01/image-22.png",
-     
-     Discription: "Dal Tadka is a popular Indian lentil dish made with arhar dalhusked & split pigeon pea lentils) or masoor dal (husked & split red lentils. This Dal Tadka recipe gives you a delicious creamy dal with smoked flavors.",
-     time: "35 mins" ,
-     categories:"Veg"
-   
-   },
-   {
-   
-     title : "Shahi paneer",
-   
-     Image:"https://cdn.tasteatlas.com//images/dishes/ceff65c140064d6285fc3814534a349b.jpg?w=905&h=510",
-    
+  {
+
+    title: "Dal Tadka",
+
+    Image: "https://www.masalabox.com/wp-content/uploads/2023/01/image-22.png",
+
+    Discription: "Dal Tadka is a popular Indian lentil dish made with arhar dalhusked & split pigeon pea lentils) or masoor dal (husked & split red lentils. This Dal Tadka recipe gives you a delicious creamy dal with smoked flavors.",
+    time: "35 mins",
+    categories: "Veg"
+
+  },
+  {
+
+    title: "Shahi paneer",
+
+    Image: "https://cdn.tasteatlas.com//images/dishes/ceff65c140064d6285fc3814534a349b.jpg?w=905&h=510",
+
     Discription: "Originating from India's Moghul cuisine, shahi paneer is a cheese curry that is prepared with paneer cheese, onions, almond paste, and a rich, spicy tomato-cream sauce. The dish is typically accompanied by Indian breads such as naan, roti, or puri.Often referred to as the Royal paneer,traditionally,it is prepared during special occasions and festivities as a vegetarian main course. Rich, hearty, and nutritious, shahi paneer is commonly garnished with coriander leaves.",
-    time: "45 mins" ,
-     categories:"Veg"
-   
-   
-   },
-   {
-   
-     title : "Paneer tikka",
-   
-     Image:"https://cdn.tasteatlas.com//images/dishes/16dbebbff2e04e0d984f4ed83be93b97.jpg?w=905&h=510",
-    
+    time: "45 mins",
+    categories: "Veg"
+
+
+  },
+  {
+
+    title: "Paneer tikka",
+
+    Image: "https://cdn.tasteatlas.com//images/dishes/16dbebbff2e04e0d984f4ed83be93b97.jpg?w=905&h=510",
+
     Discription: "This North Indian tikka variety consists of a combination of marinated paneer cheese and vegetables that are cooked in a tandoor - a traditional Indian oven. This vegetarian version of tikka is based on paneer, a crumbly cheese with a soft texture.Pieces of paneer, tomatoes, onions, and peppers are skewered, grilled, and then seasoned with lemon juice and chaat masala - a spicy powder. Sometimes the dish is served with various salads or mint chutney on the side. Variations on the dish include paneer tikka masala, which is served with spicy gravy, and paneer tikka rolls, which are wrapped in Indian bread.",
-    time: "35 mins" ,
-     categories:"Veg"
-   
-   },
-   {
-   
-     title : "Rajma",
-   
-     Image:"https://www.tasteatlas.com/Images/Dishes/25ab742da91d49b18b1a7f8a9a3f1b18.jpg?mw=1300",
-    
+    time: "35 mins",
+    categories: "Veg"
+
+  },
+  {
+
+    title: "Rajma",
+
+    Image: "https://www.tasteatlas.com/Images/Dishes/25ab742da91d49b18b1a7f8a9a3f1b18.jpg?mw=1300",
+
     Discription: "Rajma is one of the most popular vegetarian dishes of North India. It consists of red kidney beans which are stewed in a rich gravy combined with numerous spices. The dish is usually prepared for festivities and special occasions, when it is typically served alongside rice and breads such as roti.Although closely associated with North India, where it is a staple food, red kidney bean was actually brought to the country from central Mexico and Guatemala, but the Mexican version of the dish is much different than the Indian one. Hearty and nutritious, rajma remains one of the most loved vegetarian curries in Punjab and North India.",
-   time: "30 mins" ,
-    categories:"Veg"
-   
-   }
-   
-   
-     
-      
-       ]
-   
-   
-   
-   
-   
-   export default DataCard
+    time: "30 mins",
+    categories: "Veg"
+
+  }
+]
+
+export default DataCard
