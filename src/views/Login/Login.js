4ee1b4594552e0e3df8b72b81e19d564de7dde2f
sakshilambe1997 <sakshilambe1997@gmail.com>
2024-06-20 14:47:42 +0530

    changes added

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,5 +1,9 @@
+
 import "./Login.css"
 import LoginImg from "./../../assets/Login-Img/background-img-4.jpg"
+import Facebook from "./../../assets/FooterImg/facebook.png"
+import Google from "./../../assets/FooterImg/google.png"
+import Email from "./../../assets/FooterImg/gmail.png"
 
 function Login(){
     return(
@@ -7,6 +11,21 @@ function Login(){
         <img src={LoginImg} className="background-img"/>
         <div className="login-main-div">
             <p className="login-heading">Login Page</p>
+            <div className="login-logo-div">
+                <img src={Facebook} className="login-logo-img"/>
+                <img src={Google} className="login-logo-img"/>
+                <img src={Email} className="login-logo-img"/>
+
+            </div>
+
+            <div className="Form-div">
+                <form>
+                <input type="text" className="input-box" placeholder="Username"></input>
+                <input type="text" className="input-box" placeholder="Password"></input><br></br>
+                
+                </form>
+                <button type="button" className="btn">Login</button>
+            </div>
 
 
 
