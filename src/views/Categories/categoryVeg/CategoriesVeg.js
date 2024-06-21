ce4b71d112500ab9ca47928cfb616cd00618ecec
pranayrdandekar7 <pranayrdandekar7@gmail.com>
2024-06-21 15:33:20 +0530

    logical part is added for search item

--- src/views/Categories/categoryVeg/CategoriesVeg.js
+++ src/views/Categories/categoryVeg/CategoriesVeg.js
@@ -4,20 +4,40 @@ import VegCard from "./../../../components/VegCard/VegCard"
 import headerimg from "./header-img.jpg"
 import Navbar from "./../../../components/Navbar/Navbar"
 import Footer from "./../../../components/Footer/Footer"
+import Button from "../../../components/Button/Button"
+import { useState } from "react"
 
 
 function CategoriesVeg() {
+
+  const [filteredData, setFilteredData] = useState([...Data])
+  const [searchText, setSearchText] = useState("")
 
   return (
     <>
       <Navbar />
       <img src={headerimg} className="header-img" />
-      <h1 className="head-title">Veg Delicious Food</h1>
+      <span className="head-title">Veg Delicious Food</span>
+
+        <div className=" search-container d-flex align-items-center mx-auto ">
+          <input 
+            type="text" 
+            placeholder="serach" 
+            className="seachbar rounded-3 m-2 p-1 fs-5"
+            value={searchText}
+            onChange={(e)=>{
+              setSearchText(e.target.value)
+            }}
+            />
+             {setSearchText}
+        </div>
+   
+
 
 
       <div className="veg-container">
 
-        {Data.map((blogObject, i) => {
+        {filteredData.map((blogObject, i) => {
           const {
             id,
             title,
@@ -28,7 +48,10 @@ function CategoriesVeg() {
 
           } = blogObject
 
-          return (<VegCard title={title} image={image} description={description} time={time} id={id} categories={categories} />)
+          return (
+          
+          
+          <VegCard title={title} image={image} description={description} time={time} id={id} categories={categories} />)
         })}
       </div>
 
